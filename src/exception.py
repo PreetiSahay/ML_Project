96180ac795b930c912dab24853dc6a5eaac53d06
PreetiSahay <preeti.sahay511982@gmail.com>
2024-05-05 16:45:07 +0800

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,7 @@
 import sys
-#from src.logger 
-import logging
+import os
+sys.path.append(os.path.abspath('D:/House Price Prediction/src'))
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -18,15 +19,17 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-    '''
+
+
+'''
     if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by zero error")
-        raise CustomException(e,sys)
-    
-    '''
+        try:
+            a=1/0
+        except Exception as e:
+            logging.info("Divide by zero error")
+            raise CustomException(e,sys)
+'''
+
     
 
         
